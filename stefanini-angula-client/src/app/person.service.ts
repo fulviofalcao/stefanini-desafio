54edb519dd10afb7661ffc0b97e4ca36b155708d
fulvio.falcao <fulviofalcao@gmail.com>
2020-08-25 02:13:08 +0100

    Commit - stefanini-angula-client

---
+++ stefanini-angula-client/src/app/person.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+
+  private baseUrl = 'http://localhost:8080/stefanini-api-rest/api/v1/persons';
+
+  constructor(private http: HttpClient) { }
+
+  getPerson(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createPerson(person: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, person);
+  }
+
+  updatePerson(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deletePerson(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+
+  getPersonsList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+}
---
+++ stefanini-angula-client/src/app/person.ts
@@ -0,0 +1,13 @@
+export class Person {
+    id: number;
+    fullName: string;
+    gender: string;
+    emailId: string;
+    dateBirth: Date;
+    placeBirth: string;
+    nationality: string;
+    cpf: string;
+    dateRegis: Date;
+    dateUpdate: Date;
+    active: boolean;
+}
